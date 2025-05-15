0a09c77526a328c894036bc2324070edff515d73
Lakshman636 <lakshmanudaya79@gmail.com>
2025-05-15 14:10:23 +0530

    submodule changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import SignIn from './pages/Signin.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
-   <h1 className='text-red-500'>AppS</h1>
+   <BrowserRouter>
+   <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Sign-In" element={<SignIn />} />
+      <Route path="/Sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      
+    </Routes>
+   </BrowserRouter>
   )
 }
+ 
