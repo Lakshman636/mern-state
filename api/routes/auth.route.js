64ccb3453e35b424075f698de76a6b2c9d7b0987
Lakshman636 <lakshmanudaya79@gmail.com>
2025-05-19 20:18:03 +0530

    create user update api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { google, signin, signup } from '../controllers/auth.controller.js';
+import { updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/signin",signin);
 router.post('/google',google);
+router.post("/update/:id", updateUser);
 export default router;
